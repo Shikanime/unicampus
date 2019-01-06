876203ef4eea71286eb11d23530fc94bdc6f4aec
Shikanime <deva.shikanime@protonmail.com>
2019-01-06 18:18:55 +0100

    chore: lock deps

---
+++ cmd/account/sever.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type Server struct {
+}
+
+func NewTCPListener() net.Listener {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "50051"
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	return listener
+}
+
+func main() {
+	tcpListener := NewTCPListener()
+
+	// Server
+	s := grpc.NewServer()
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(tcpListener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
