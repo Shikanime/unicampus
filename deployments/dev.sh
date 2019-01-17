af0cf08351c808d65c6b8ed998e9c667bef1a50c
Shikanime <deva.shikanime@protonmail.com>
2019-01-17 09:56:20 +0100

    chore: move deployment to helm chart umbrella

--- deployments/dev.sh
+++ deployments/dev.sh
@@ -1,3 +1,3 @@
-kubectl port-forward --namespace default svc/unicampus-postgresql 5432:5432
-kubectl port-forward --namespace default svc/unicampus-elasticsearch-client 9200:9200
-kubectl port-forward --namespace default svc/unicampus-neo4j 7474:7474
+kubectl port-forward --namespace default svc/unicampus-postgresql 5432:5432 &
+kubectl port-forward --namespace default svc/unicampus-elasticsearch-client 9200:9200 &
+kubectl port-forward --namespace default svc/unicampus-neo4j 7474:7474 &
