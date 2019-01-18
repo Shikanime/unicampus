abad2de7438f66dce66f88d11188ef66771ef879
Shikanime Deva <deva.shikanime@protonmail.com>
2019-01-18 23:00:46 +0100

    chore: neo4j env var

--- internal/pkg/services/neo4j.go
+++ internal/pkg/services/neo4j.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	neo4j "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 )
@@ -10,9 +12,38 @@ type Neo4jService struct {
 	driver neo4j.Conn
 }
 
+func lookupNeo4jUsername() string {
+	username, ok := os.LookupEnv("NEO4J_USERNAME")
+	if !ok {
+		username = "postgres"
+	}
+	return username
+}
+
+func lookupNeo4jPassword() string {
+	password, ok := os.LookupEnv("NEO4J_PASSWORD")
+	if !ok {
+		password = "unicampus"
+	}
+	return password
+}
+
+func lookupNeo4jHost() string {
+	host, ok := os.LookupEnv("NEO4J_HOST")
+	if !ok {
+		host = "localhost"
+	}
+	return host
+}
+
 func NewNeo4jService() *Neo4jService {
 	driver := neo4j.NewDriver()
-	conn, err := driver.OpenNeo("bolt://neo4j:neosecret@localhost:7687")
+	conn, err := driver.OpenNeo(fmt.Sprintf(
+		"bolt://%s:%s@%s:7687",
+		lookupNeo4jUsername(),
+		lookupNeo4jPassword(),
+		lookupNeo4jHost(),
+	))
 	if err != nil {
 		log.Fatalf("failed to connect neo4j service")
 	}
