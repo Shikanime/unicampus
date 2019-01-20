b1ba55be556ea3f7784746525eff2cb4e79cf405
Shikanime Deva <deva.shikanime@protonmail.com>
2019-01-20 16:25:19 +0100

    chore: add command to makefile

--- deployments/kubernetes/istio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "deploy" = $1 ]; then
-  kubectl apply -f ./third_party/istio/install/kubernetes/helm/helm-service-account.yaml
-  helm install ./third_party/istio/install/kubernetes/helm/istio --name istio --namespace istio-system
-elif [ "clean" = $1 ]; then
-  kubectl delete -f $ISTIO_CHART_PATH/helm-service-account.yaml
-  kubectl -n istio-system delete job --all
-fi
