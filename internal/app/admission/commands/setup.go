9abda2cc8198a8393898cf823e80a9bb25173a4f
Shikanime Deva <deva.shikanime@protonmail.com>
2019-01-22 00:32:24 +0100

    fix: uuid

--- internal/app/admission/commands/setup.go
+++ internal/app/admission/commands/setup.go
@@ -4,6 +4,8 @@ import (
 	"github.com/Shikanime/unicampus/internal/app/admission/repositories/indexer"
 	"github.com/Shikanime/unicampus/internal/app/admission/repositories/persistence"
 	"github.com/Shikanime/unicampus/internal/pkg/services"
+	"github.com/Shikanime/unicampus/pkg/admission"
+	uuid "github.com/satori/go.uuid"
 	"github.com/spf13/cobra"
 )
 
@@ -28,6 +30,18 @@ func NewSetup(appName string) *cobra.Command {
 			if err = indexerRepo.Init(); err != nil {
 				panic(err)
 			}
+
+			persistenceRepo.CreateSchool(&admission.School{
+				UUID:        uuid.NewV4().String(),
+				Name:        "ETNA",
+				Description: "The École des technologies numériques appliquées (ETNA) is a French private school in computer science localized at Ivry-sur-Seine. Created in 2005 by Patrice Dumoucel, the school since 2006 is part of IONIS Education Group. The certification delivered by the school are recognized by the French state.",
+			})
+
+			persistenceRepo.CreateSchool(&admission.School{
+				UUID:        uuid.NewV4().String(),
+				Name:        "ESGI",
+				Description: ".",
+			})
 		},
 	}
 
--- internal/pkg/utils/uuid.go
+++
@@ -1,7 +0,0 @@
-package util
-
-import uuid "github.com/satori/go.uuid"
-
-func NewUUID() uuid.UUID {
-	return uuid.Must(uuid.NewV4())
-}
