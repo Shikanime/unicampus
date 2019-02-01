1966db76b31ba53544a3ab74fdf508b36fe963d2
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-01 21:18:42 +0100

    chore: add elixir grpc artifacts

---
+++ packages/elixir/apps/unicampus/lib/unicampus.ex
@@ -0,0 +1,2 @@
+defmodule Unicampus do
+end
--- scripts/protobuf/build.sh
+++ scripts/protobuf/build.sh
@@ -8,4 +8,5 @@ protoc \
   -I$(go env GOPATH)/src \
   -I$(go env GOPATH)/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --gofast_out=plugins=grpc:./packages/go \
+  --elixir_out=plugins=grpc:./packages/elixir/apps/unicampus/lib/unicampus \
   ./api/education/v1alpha1/generated.proto
