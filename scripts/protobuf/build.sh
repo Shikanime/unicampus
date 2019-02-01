1dfc0156af712ffc05d9366f61cd95c028339a68
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-01 21:23:25 +0100

    chore: add grpc to web client

--- scripts/protobuf/build.sh
+++ scripts/protobuf/build.sh
@@ -9,4 +9,6 @@ protoc \
   -I$(go env GOPATH)/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --gofast_out=plugins=grpc:./packages/go \
   --elixir_out=plugins=grpc:./packages/elixir/apps/unicampus/lib/unicampus \
+  --js_out=import_style=commonjs:./packages/javascript/packages/web-client/src \
+  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./packages/javascript/packages/web-client/src \
   ./api/education/v1alpha1/generated.proto
