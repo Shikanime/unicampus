594c10200a920dbcdb8906068b10185566012f13
Shikanime Deva <deva.shikanime@protonmail.com>
2019-02-02 20:11:16 +0100

    chore: remove elixir

--- packages/elixir/apps/education/config/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :education, Education.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "education_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
